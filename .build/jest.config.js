376d729ddf5b631015c7567c9d06714a792f1475
Igor Kulishov <igorkulishov@gmail.com>
2021-12-08 22:16:37 -0500

    SCED-28: remove build folder

--- .build/jest.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node'
-};
-//# sourceMappingURL=jest.config.js.map
